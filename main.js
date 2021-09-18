c4d192a33539f16ddc23ca169bf5080c672e41e3
Arunangshu009 <83114829+Arunangshu009@users.noreply.github.com>
2021-09-18 12:13:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+Webcam.set({
+    width:350,
+    height:350,
+    image_format:'png',
+    png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach('#Camera')
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    })
+}
+
+function speak(){
+
+    var synth = window.speechSynthesis;
+    
+    speak_data_1 = "The first prediction is " + prediction_1;
+    
+    speak_data_2 = "And the second prediction is " + prediction_2;
+    
+    var utterThis = new SpeechSynthesisUtterance (speak_data_1 + speak_data_2);
+    
+    synth.speak (utterThis);
+}
